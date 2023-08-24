cccf3703b45e93d63e0dcb8df7b6895d14b32f43
Alexander Walther <aw@alexplus.de>
2023-08-24 10:49:25 +0200

    initial commit

---
+++ lib/yform_model_class.php
@@ -0,0 +1,46 @@
+<?php
+
+class ymca
+{
+    public static function toCamelCase($input)
+    {
+        return str_replace(' ', '', ucwords(str_replace('_', ' ', $input)));
+    }
+    public static function toClassName($input)
+    {
+        return str_replace('rex_', '', $input);
+    }
+
+    public static function getTypeTemplate($type_name)
+    {
+        $typeTemplates = [
+            'value' => '
+    /** @api */
+    public function get%s() : ?%s {
+        return $this->getValue("%s");
+    }
+            ',
+            'datetime' => '
+    /** @api */
+    public function get%s() : ?string {
+        return $this->getValue("%s");
+    }
+            ',
+            'relation' => '
+    /** @api */
+    public function get%1$s() : ?rex_yform_manager_dataset {
+        return $this->getRelatedDataset("%3$s");
+    }
+            ',
+            'collection' => '
+    /** @api */
+    public function get%1$s() : ?rex_yform_manager_collection {
+        return $this->getRelatedCollection("%3$s");
+    }
+            ',
+        ];
+
+        return $typeTemplates[$type_name] ?? '';
+    }
+
+}
---
+++ pages/yform.yform_model_class.php
@@ -0,0 +1,87 @@
+<div class="row">
+	<div class="col-12">
+
+		<?php
+
+echo \rex_view::title(\rex_i18n::msg('yform'));
+
+
+		$tables = ['rex_staff', 'rex_staff_category'];
+
+		$classTemplate = '
+class %s extends \rex_yform_manager_dataset {
+	%s
+}';
+		$methodTemplate = '
+	public function get%s() : ?%s {
+		return $this->getValue("%s");
+	}
+';
+
+		foreach ($tables as $table) {
+
+		    $results = rex_sql::factory()->getArray("SELECT `id`, `table_name`, `prio`, `type_name`, `type_id`, `db_type`, `name`, `label` FROM `rex_yform_field` WHERE `type_name` != 'validate' AND `table_name` = '$table' ORDER BY `prio`");
+
+		    $generatedClasses = '';
+
+		    foreach ($results as $result) {
+
+		        if ($result['type_name'] === 'fieldset') {
+		            continue;
+		        }
+                
+		        if ($result['type_name'] === 'html') {
+		            continue;
+		        }
+                
+		        if ($result['type_id'] === 'value') {
+		            $className = ymca::toClassName($result['table_name']);
+		            $methodName = ymca::toCamelCase($result['name']);
+
+		            // Mapping der db_type zu PHP-Typen
+		            $methodMap = [
+		                'text' => 'value',
+		                'datetime' => 'datetime',
+		                'be_manager_relation' => 'relation',
+		                'be_manager_collection' => 'collection',
+		                // ... weitere Typen hier hinzufügen
+		            ];
+		            $defaultMethod = 'value';
+                    
+
+		            $methodTemplate = ymca::getTypeTemplate($methodMap[$result['type_name']] ?? $defaultMethod);
+
+		            // Default-Typ, falls kein passender db_type gefunden wird
+		            $typeMap = [
+		                'varchar(191)' => 'string',
+		                'text' => 'string',
+		                'tinyint(1)' => 'bool',
+		                'datetime' => '\DateTime',
+		                // ... weitere Typen hier hinzufügen
+		            ];
+		            $defaultType = 'mixed';
+
+		            $returnType = $typeMap[$result['db_type']] ?? $defaultType;
+
+		            $generatedClasses .= sprintf(
+		                $methodTemplate,
+		                $methodName,
+		                $returnType,
+		                $result['name']
+		            );
+		        }
+                
+		    }
+
+
+
+		    $fullCode = sprintf($classTemplate, $className, $generatedClasses);
+
+		    echo '<pre class="pre-scrollable">'.$fullCode.'</pre>';
+
+		}
+
+		?>
+
+	</div>
+</div>
