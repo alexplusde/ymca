8d6bf975f56b340fa719daf5c81d1153aadb02e6
Alexander Walther <mail@alexplus.de>
2023-12-22 16:58:51 +0100

    Update yform_model_class.php

--- lib/yform_model_class.php
+++ lib/yform_model_class.php
@@ -1,229 +1,145 @@
-<?php
+<div class="row">
+	<div class="col-12">
 
-class ymca
-{
-    public static function toCamelCase($input)
-    {
-        return str_replace(' ', '', ucwords(str_replace('_', ' ', $input)));
-    }
-    public static function toClassName($input)
-    {
-        return str_replace('rex_', '', $input);
-    }
+		<?php
 
-    public static function getTypeTemplate($type_name)
-    {
-        $typeTemplates =
-[
-'value' => '
-    /* %4$s */
-    /** @api */
-    public function get%1$s() : %2$s {
-        return $this->getValue("%3$s");
-    }
-    /** @api */
-    public function set%1$s(mixed $value) : self {
-        $this->setValue("%3$s", $value);
-        return $this;
-    }
-',
-'checkbox' => '
-    /* %4$s */
-    /** @api */
-    public function get%1$s(bool $asBool = false) : mixed {
-        if($asBool) {
-            return (bool) $this->getValue("%3$s");
-        }
-        return $this->getValue("%3$s");
-    }
-    /** @api */
-    public function set%1$s(int $value = 1) : self {
-        $this->setValue("%3$s", $value);
-        return $this;
-    }
-            ',
-'textarea' => '
-    /* %4$s */
-    /** @api */
-    public function get%1$s(bool $asPlaintext = false) : %2$s {
-        if($asPlaintext) {
-            return strip_tags($this->getValue("%3$s");
-        }
-        return $this->getValue("%3$s");
-    }
-    /** @api */
-    public function set%1$s(mixed $value) : self {
-        $this->setValue("%3$s", $value);
-        return $this;
-    }
-            ',
-'datetime' => '
-    /* %4$s */
-    /** @api */
-    public function get%1$s() : ?string {
-        return $this->getValue("%3$s");
-    }
-    /** @api */
-    public function set%1$s(string $datetime) : self {
-        $this->getValue("%3$s", $datetime);
-        return $this;
-    }
-            ',
-'be_media' => '
-    /* %4$s */
-    /** @api */
-    public function get%1$s(bool $asMedia = false) : ?string {
-        if($asMedia) {
-            return rex_media::get($this->getValue("%3$s"));
-        }
-        return $this->getValue("%3$s");
-    }
-    /** @api */
-    public function set%1$s(string $filename) : self {
-        if(rex_media::get($filename)) {
-            $this->getValue("%3$s", $filename);
-        }
-        return $this;
-    }
-            ',
-'datestamp' => '
-    /* %4$s */
-    /** @api */
-    public function get%1$s() : ?string {
-        return $this->getValue("%3$s");
-    }
-    /** @api */
-    public function set%1$s(string $value) : self {
-        $this->setValue("%3$s", $value);
-        return $this;
-    }
-',
-'integer' => '
-    /* %4$s */
-    /** @api */
-    public function get%1$s() : ?integer {
-        return $this->getValue("%3$s");
-    }
-    /** @api */
-    public function set%1$s(int $value) : self {
-        $this->setValue("%3$s", $value);
-        return $this;
-    }
-',
-'number' => '
-    /* %4$s */
-    /** @api */
-    public function get%1$s() : ?float {
-        return $this->getValue("%3$s");
-    }
-    /** @api */
-    public function set%1$s(float $value) : self {
-        $this->setValue("%3$s", $value);
-        return $this;
-    }
-            ',
-'prio' => '
-    /* %4$s */
-    /** @api */
-    public function get%1$s() : ?int {
-        return $this->getValue("%3$s");
-    }
-    /** @api */
-    public function set%1$s(int $value) : self {
-        $this->setValue("%3$s", $value);
-        return $this;
-    }
-            ',
-'time' => '
-    /* %4$s */
-    /** @api */
-    public function get%1$s() : string {
-        return $this->getValue("%3$s");
-    }
-    /** @api */
-    public function set%1$s(string $value = "00:00") : self {
-        $this->setValue("%3$s", $value);
-        return $this;
-    }
-            ',
-'domain' => '
-    /* %4$s */
-    /** @api */
-    public function get%1$s() : ?yrewrite_domain {
-        return yrewrite_domain::get($this->getValue("%3$s"));
-    }
-    /** @api */
-    public function get%1$sId() : ?int {
-        return $this->getValue("%3$s");
-    }
-    /** @api */
-    public function set%1$s(int $value) : self {
-        $this->setValue("%3$s", $value);
-        return $this;
-    }
-',
-'be_user' => '
-    /* %4$s */
-    /** @api */
-    public function get%1$s() : ?rex_user {
-        return rex_user::get($this->getValue("%3$s"));
-    }
-    /** @api */
-    public function set%1$s(mixed $value) : self {
-        $this->setValue("%3$s", $value);
-        return $this;
-    }
-',
-'be_link' => '
-    /* %4$s */
-    /** @api */
-    public function get%1$s(bool $asArticle = false) : ?rex_article {
-        return rex_article::get($this->getValue("%3$s"));
-    }
-    public function get%1$sId() : ?int {
-        return $this->getValue("%3$s");
-    }
-    public function get%1$sUrl() : ?string {
-        if($article = $this->get%1$s()) {
-            return $article->getUrl();
-        }
-    }
-    /** @api */
-    public function set%1$s(string $id) : self {
-        if(rex_article::get($id)) {
-            $this->getValue("%3$s", $id);
-        }
-        return $this;
-    }
-',
-'relation' => '
-    /* %4$s */
-    /** @api */
-    public function get%1$s() : ?rex_yform_manager_dataset {
-        return $this->getRelatedDataset("%3$s");
-    }
-',
-'collection' => '
-    /** @api */
-    public function get%1$s() : ?rex_yform_manager_collection {
-        return $this->getRelatedCollection("%3$s");
-    }
-',
-'choice' => '
-    /* %4$s */
-    /** @api */
-    public function get%1$s() : ?mixed {
-        return $this->getValue("%3$s");
-    }
-    /** @api */
-    public function set%1$s(mixed $param) : ?mixed {
-        $this->setValue("%3$s", $param);
-        return $this;
-    }
-',
-];
+echo \rex_view::title(\rex_i18n::msg('yform'));
 
-        return $typeTemplates[$type_name] ?? '';
-    }
 
-}
+		$tables = rex_sql::factory()->getArray("SELECT table_name FROM rex_yform_table ORDER BY table_name");
+		$t = [];
+		foreach ($tables as $table) {
+		    $t[] = $table['table_name'];
+		}
+
+		$bootTemplate = '
+if (rex_addon::get(\'yform\')->isAvailable() && !rex::isSafeMode()) {
+	rex_yform_manager_dataset::setModelClass(
+		\'rex_%1$s\',
+		%1$s::class,
+	);
+}'
+		;
+		$classTemplate = '
+class %1$s extends \rex_yform_manager_dataset {
+	%2$s
+}';
+
+
+		foreach ($t as $table) {
+
+		    $results = rex_sql::factory()->getArray("SELECT `id`, `table_name`, `prio`, `type_name`, `type_id`, `db_type`, `name`, `label` FROM `rex_yform_field` WHERE `type_name` != 'validate' AND `table_name` = '$table' ORDER BY `prio`");
+
+		    $generatedClasses = '';
+
+		    foreach ($results as $result) {
+
+		        if ($result['type_name'] === 'fieldset') {
+		            continue;
+		        }
+                
+		        if ($result['type_name'] === 'html') {
+		            continue;
+		        }
+                
+		        if ($result['type_id'] === 'value') {
+		            $className = ymca::toClassName($result['table_name']);
+		            $methodName = ymca::toCamelCase($result['name']);
+
+		            // Mapping der db_type zu PHP-Typen
+		            $methodMap = [
+		                'be_link' => 'be_link',
+		                'be_manager_relation' => 'relation',
+		                'be_manager_collection' => 'collection',
+		                'be_media' => 'be_media',
+		                'be_media_preview' => 'be_media',
+		                'be_user' => 'be_user',
+		                'checkbox' => 'checkbox',
+		                'choice_status' => 'choice',
+		                'datestamp' => 'datestamp',
+		                'datetime' => 'datetime',
+		                'domain' => 'domain',
+		                'integer' => 'integer',
+		                'number' => 'number',
+		                'prio' => 'integer',
+		                'text' => 'value',
+		                'textarea' => 'textarea',
+		                'time' => 'time',
+		            ];
+		            $defaultMethod = 'value';
+                    
+
+		            $methodTemplate = ymca::getTypeTemplate($methodMap[$result['type_name']] ?? $defaultMethod);
+
+		            // Default-Typ, falls kein passender db_type gefunden wird oder mehrere zulässig sind.
+		            $typeMap = [
+		                'bigint' => 'integer',
+		                'date' => '?string',
+		                'datetime' => '?\DateTime',
+		                'int' => 'integer',
+		                'tinyint(1)' => '?bool',
+		                'text' => '?string',
+		                'varchar(191)' => '?string',
+		            ];
+		            $defaultType = 'mixed';
+
+		            $returnType = $typeMap[$result['db_type']] ?? $defaultType;
+
+		            if (strpos($result['label'], 'translate:') === 0) {
+		                $translationKey = substr($result['label'], strlen('translate:'));
+		                $result['label'] = rex_i18n::msg($translationKey);
+		            }
+
+		            $generatedClasses .= sprintf(
+		                $methodTemplate,
+		                $methodName,
+		                $returnType,
+		                $result['name'],
+		                $result['label'],
+		                ""
+		            );
+		        }
+                
+		    }
+
+		    $bootCode = sprintf($bootTemplate, $className);
+		    $classCode = sprintf($classTemplate, $className, $generatedClasses);
+		    ?>
+
+		<section class="rex-page-section">
+
+
+			<div class="panel panel-default">
+
+				<header class="panel-heading">
+					<div class="panel-title"><?= $table ?></div>
+				</header>
+
+				<div class="panel-body">
+					<p># 1. In die <code>boot.php</code> muss folgender auskommentierte Code:</p>
+					<pre class="pre-scrollable">
+						<?= $bootCode ?>
+					</pre>
+
+					<p># 2. Erstelle eine Datei
+						<code>lib/<?= $className ?>.php</code> im
+						<code>project</code>-Addon oder
+						eigenen Addon mit folgendem Inhalt:
+					</p>
+
+					<pre class="pre-scrollable">
+						<?= $classCode ?>
+					</pre>
+				</div>
+			</div>
+
+
+		</section>
+
+		<?php
+		}
+
+		?>
+
+	</div>
+</div>
